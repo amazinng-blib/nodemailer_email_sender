1e7dd467470e7e0ab09966adb290e3b3d64ed3d4
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2023-11-01 12:53:28 +0100

    nodemailer api

---
+++ email.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer');
+
+const sendMessage = async (email, name, message) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      host: 'smpt.gmail.com',
+      // port: 465,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const options = {
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: 'Get In-Touch',
+
+      html: `
+            <div style="width: 100%; max-width: 500px; display: block; margin: auto">
+            <h2>From ${name}</h2>
+              <p>
+               ${message}
+              </p>
+            </section>
+          </div>
+        
+            `,
+    };
+
+    const emailSent = await transporter.sendMail(options);
+    console.log(emailSent);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = sendMessage;
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const sendMessage = require('./email');
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+app.post('/send-mail', async (req, res) => {
+  try {
+    console.log({ req: req?.body });
+    const { name, email, message } = req?.body;
+    if ((name, email, message)) {
+      sendMessage(email, name, message);
+      return res.status(200).json({ message: 'Message Sent' });
+    }
+
+    return res
+      .status(400)
+      .json({ message: 'Please, fill-out the form and send' });
+  } catch (error) {
+    res.status(500).json(error?.message);
+  }
+});
+
+const PORT = process.env.PORT || 5002;
+
+app.listen(PORT, () => {
+  console.log(`Server connected at http://localhost:${PORT}`);
+});
