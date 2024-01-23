b0db00edeb7bd9c40953c8cbf93c753861755306
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2024-01-23 10:54:31 +0100

    consoled req.body

--- routes/EmialRoutes.js
+++ routes/EmialRoutes.js
@@ -3,8 +3,11 @@ const express = require('express');
 const route = express.Router();
 
 route.post('/send-mail', async (req, res) => {
+  console.log({ first });
   try {
     const { name, email, message } = req?.body;
+    console.log({ name, email, message });
+
     if ((name, email, message)) {
       sendMessage(email, name, message);
       return res.status(200).json({ message: 'Message Sent' });
